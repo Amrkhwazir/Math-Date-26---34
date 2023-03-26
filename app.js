9779a5e1e2a2c3ed8d42c03629be7f51d6d5421e
Amirkhan567 <amrkh5678@gmail.com>
2023-03-26 22:09:30 +0500

    math and date practice

---
+++ app.js
@@ -0,0 +1,120 @@
+// QNO : 01
+
+// let num = 45
+// console.log(num)
+// console.log(`round value ${Math.round(num)}`)
+// console.log(`floor value ${Math.floor(num)}`)
+// console.log(`ceil value ${Math.ceil(num)}`)
+
+// QNO : 02
+
+// let num = -32
+// console.log(num)
+// console.log(`round value ${Math.round(num)}`)
+// console.log(`floor value ${Math.floor(num)}`)
+// console.log(`ceil value ${Math.ceil(num)}`)
+
+// QNO : 03
+
+// let num = 3.45124
+// console.log(num)
+// console.log(`round value ${Math.round(num)}`)
+// console.log(`floor value ${Math.floor(num)}`)
+// console.log(`ceil value ${Math.ceil(num)}`)
+
+// QNO : 04
+
+// let num = -2.657
+// console.log(num)
+// console.log(`round value ${Math.round(num)}`)
+// console.log(`floor value ${Math.floor(num)}`)
+// console.log(`ceil value ${Math.ceil(num)}`)
+
+// QNO : 05
+
+// let num = -4
+// console.log(num)
+// console.log("absolute value " + Math.abs(num))
+
+
+// let num = -5
+// console.log(num)
+// console.log("absolute value " + Math.abs(num))
+
+
+// QNO : 06
+
+// console.log(Math.round(Math.random()*5+1))
+
+// QNO : 07
+
+// let value = Math.round(Math.random()*2)
+// if(value == 2){
+//     console.log('head')
+// }else{
+//     console.log('tail')
+    
+// }
+
+// QNO : 08
+
+// let num = Math.round(Math.random()*100)
+// console.log(num)
+
+// QNO : 09
+
+// var input = prompt("wnter your weight")
+// console.log(input + " kilograms")
+
+// QNO : 10
+
+// let num = 3
+// var user = prompt("enter a correct number")
+// if(user == num){
+//     console.log("Congratulation")
+// }else{
+//     console.log("Sorry try again!")
+// }
+
+// QNO : 11
+
+// var day = new Date()
+// console.log(day)
+
+// QNO : 12
+
+// var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+// let d = new Date();
+// let month = months[d.getMonth()];
+// console.log(month)
+
+// QNO : 13
+
+// var days = ['sun','mon','tues','wed','thurs','fri','sat'];
+// let d = new Date();
+
+// console.log(days[d.getDay()])
+
+// QNO : 14
+
+// var days = ['sun','mon','tues','wed','thurs','fri','sat'];
+// let d = new Date();
+// let day = days[d.getDay()]
+
+// if(day == "sun" || "sat"){
+//     console.log('today is funday')
+// }else{
+//     console.log('today is not funday')
+
+// }
+
+// QNO : 15
+
+// let d = new Date().getDate();
+// console.log(d)
+// if(d < 15){
+// console.log('First fifteen days of the month')
+// }else{
+// console.log('Last days of the month')
+// }
+
